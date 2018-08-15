17f392f6c743eea73613bafb27fa899fc44a319d
Dídac García <didaquis@gmail.com>
2018-08-15 12:37:02 +0200

    Using JSDoc for comment Users class

--- server/classes/users.js
+++ server/classes/users.js
@@ -1,8 +1,17 @@
+/** Class representing an user */
 class Users {
+	/**
+     * Constructor method
+     */
 	constructor() {
 		this.people = [];
 	}
 
+	/**
+     * Add an user
+     * @param {string} id 	Id of user
+     * @param {string} name Name of user
+     */
 	addPerson(id, name) {
 		let person = { id, name };
 
@@ -11,6 +20,11 @@ class Users {
 		return this.people;
 	}
 
+	/**
+	 * Get a person by id
+	 * @param  {string} id Id of user
+	 * @return {object}    Object with user data
+	 */
 	getPerson(id) {
 		let person = this.people.filter( p => {
 			return p.id === id;
@@ -19,12 +33,29 @@ class Users {
 		return person; // si no hay resultados, devolverá "undefined".
 	}
 
+	/**
+	 * Get all persons
+	 * @return {Array.<Object>} Array of objects with data of persons
+	 */
 	getPeople() {
 		return this.people;
 	}
 
-	deletePerson(id) {
+	/**
+	 * Get all persons by room
+	 * @param {string} room Id of room
+	 * @return {Array.<Object>} Array of objects with data of persons
+	 */
+	getPeopleByRoom(room) {
+		// ...
+	}
 
+	/**
+	 * Delete a person from people list
+	 * @param  {string} id Id of user
+	 * @return {object}    Object with deleted user data
+	 */
+	deletePerson(id) {
 		let deletedPerson = this.getPerson(id);
 
 		this.people = this.people.filter(p => {
@@ -35,4 +66,9 @@ class Users {
 	}
 }
 
+/**
+ * Users class.
+ * @module Users
+ * @class
+ */
 module.exports = { Users };
